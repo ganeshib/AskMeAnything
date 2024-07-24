8df3bc5408d736d049a2aad0d413f61f05de4608
Ganesh <ganeshib08@gmail.com>
2024-07-24 10:21:23 +0530

    Added loading functionality using coroutines

--- app/src/main/java/com/example/askmeanything/MainActivity.kt
+++ app/src/main/java/com/example/askmeanything/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.askmeanything
 
-
-import android.app.ProgressDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -13,10 +11,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.transition.Visibility
-import com.google.ai.client.generativeai.BuildConfig
 import com.google.ai.client.generativeai.GenerativeModel
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,29 +28,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val etPrompt=findViewById<EditText>(R.id.editTextText)
-        val btnSub=findViewById<Button>(R.id.button)
-        val tvResult=findViewById<TextView>(R.id.result)
-        val pgBar=findViewById<ProgressBar>(R.id.progressBar)
-        pgBar.visibility=View.GONE
+        val etPrompt = findViewById<EditText>(R.id.editTextText)
+        val btnSub = findViewById<Button>(R.id.button)
+        val tvResult = findViewById<TextView>(R.id.result)
+        val pgBar = findViewById<ProgressBar>(R.id.progressBar)
+        pgBar.visibility = View.GONE
+
         btnSub.setOnClickListener {
-            val prompt=etPrompt.text.toString()
-            if(prompt.isEmpty()){
-                Toast.makeText(this,"Enter Text To Search",Toast.LENGTH_SHORT).show()
-            }else{
-                pgBar.visibility=View.VISIBLE
+            val prompt = etPrompt.text.toString()
+            if (prompt.isEmpty()) {
+                Toast.makeText(this, "Enter Text To Search", Toast.LENGTH_SHORT).show()
+            } else {
+                pgBar.visibility = View.VISIBLE
                 val generativeModel = GenerativeModel(
                     // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
                     modelName = "gemini-1.5-flash",
                     // Access your API key as a Build Configuration variable (see "Set up your API key" above)
-                    apiKey="AIzaSyBryEXtLzMS3uK3m1aGCIzgsct9Zm3COUY"
+                    apiKey = ""
                 )
 
-                runBlocking {
-                    val response = generativeModel.generateContent(prompt)
-                    tvResult.text=response.text.toString()
+                CoroutineScope(Dispatchers.Main).launch {
+                    try {
+                        val response = withContext(Dispatchers.IO) {
+                            generativeModel.generateContent(prompt)
+                        }
+                        tvResult.text = response.text.toString()
+                    } catch (e: Exception) {
+                        Toast.makeText(this@MainActivity, "Error generating content", Toast.LENGTH_SHORT).show()
+                    } finally {
+                        pgBar.visibility = View.GONE
+                    }
                 }
-                if(tvResult.text.isNotEmpty()) pgBar.visibility=View.GONE
             }
         }
     }
